e1b5ea09506333070e034d66b66346333c417b3f
JoelCabrera-irnma <140646912+JoelCabrera-irnma@users.noreply.github.com>
2023-09-13 15:51:06 -0300

    Add button Reset, New Grilla, Color Red y Black

--- script.js
+++ script.js
@@ -1,40 +1,62 @@
+//Agregar Boton de Reset, Agregar Button Color blue, add button random color y
+//add button color degradez -10%
+let colorTrazo = 'black'
 let click = true;
 
 const clicker = document.querySelector('.click')
 clicker.addEventListener('click',createSquare)
 
 
-document.querySelector('body'),addEventListener('click',function(e){
-    if(e.target.tagName !== "BUTTON"){
+document.querySelector('body').addEventListener('click',function(e){
+    if(e.target.nodeName !== "BUTTON"){
         click = !click
     }
 })
-
  
+let input = prompt('Ingresa un numero de lado de cuadricula')
+const contenedor = document.querySelector('.contenedor');
+
+
+
 function createSquare(){
-    let input = prompt('Ingresa un numero de lado de cuadricula')
-    const contenedor = document.querySelector('.contenedor');
+    contenedor.innerHTML = ""
     contenedor.style.gridTemplateColumns = `repeat(${input},1fr)`;
     contenedor.style.gridTemplateRows = `repeat(${input},1fr)`;
-    
-    console.log(input)
-    for(let i=0; i<input*input; i++){
-        
+    for(let i=0; i<input*input; i++){    
         const squareDiv = document.createElement('div');
-        //squareDiv.classList.add('square');
+        squareDiv.classList.add('square');
         contenedor.appendChild(squareDiv);
-        
         squareDiv.addEventListener('mouseover', trazo
         );
     }
 }
 
-function trazo(){
-    if(click){ 
-    this.classList.add('newColor');}
+document.querySelector('#red').addEventListener('click',changeColor)
+document.querySelector('#black').addEventListener('click',changeColor)
+
+
+function changeColor(e){
+    //console.log(e)
+    if (e.srcElement.id === "black"){
+        colorTrazo = 'black'
+    }
+    else if (e.srcElement.id === "red"){
+        colorTrazo = 'red'
+    }
 }
 
+function trazo(){
+    if(click){
+        if (colorTrazo==='black') {this.style.backgroundColor = 'black'} //classList.add('newColor');
+    } 
+    else if(colorTrazo==='red'){this.style.backgroundColor = 'red'} //classList.add('colorRed')
+}
 
-/*() => 
-        {squareDiv.classList.add('newColor');
-            }*/
+document.querySelector('#reset').addEventListener('click',createSquare)
+
+document.querySelector('.newGrilla').addEventListener('click',newInput)
+
+function newInput(){
+    input = prompt('Ingresa un nuevo valor de lado de cuadricula');
+    createSquare()
+}
